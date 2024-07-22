00e516afa37db2c739e5448bead06687c09e4dae
marioesturo <marioesturo@gmail.com>
2024-07-22 20:13:46 +0200

    Creacion del proyecto con el properties listo para usar la Base de datos

---
+++ src/main/java/com/aliAwards/AliAwardsApplication.java
@@ -0,0 +1,13 @@
+package com.aliAwards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliAwardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliAwardsApplication.class, args);
+	}
+
+}
